bc5cc9eac7a4dd4e5f4d9750e7a4791e35916011
Admin <Admin@LAPTOP-BJM79S7J>
2021-03-09 12:19:41 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -15,7 +15,18 @@ public class Libretto {
 	}
 	
 	public String toString() {
-		return this.voti.toString();
+		String s ="";
+		for(Voto v:this.voti) {
+			s=s+v.toString()+"\n";
+		}
+		return s;
 	}
-	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato= new Libretto();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		}
+		return risultato;
+	}
 }
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -10,8 +10,12 @@ public class TestLibretto {
 		Libretto libretto = new Libretto();
 		Voto voto1= new Voto("Analisi 1", 30, LocalDate.of(2019, 2, 15));
 		libretto.add(voto1);
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 9, 15)));
 		libretto.add(new Voto("Fisica 1", 28, LocalDate.of(2019, 7, 15)));
 		libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 2, 8)));
 		System.out.println(libretto);
+		
+		Libretto venticinque= libretto.votiUguali(25);
+		System.out.println(venticinque);
 	}
 }
